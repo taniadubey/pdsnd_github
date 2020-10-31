9cfb7655479ebc969ff9fd707e05b47876b5782f
Tania Dubey <tania.dubey@gmail.com>
2020-10-31 19:47:21 -0400

    Added a comment on CITY_DATA dictionary

--- bikeshare.py
+++ bikeshare.py
@@ -3,6 +3,7 @@ import time
 import pandas as pd
 import numpy as np
 
+# creating a dictionary for csv files
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
